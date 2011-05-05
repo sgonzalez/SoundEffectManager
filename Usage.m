13547945c6753b759e48d2f824550261a3c716e1
Santiago Gonzalez <santiagogonzalez@me.com>
2011-05-05 09:02:06 -0600

    Removed Comment Header

--- Usage.m
+++ Usage.m
@@ -1,10 +1,4 @@
-//
-//  Usage.m
-//
-//  Copyright 2011 Hicaduda. All rights reserved.
-//
-
-#import "SoundEffectManager.h" //Link against AudioToolbox.framework in project
+#import "SoundEffectManager.h" //Also link against AudioToolbox.framework in project
 
 //Playing a sound (chaches if not already chached)
 [SoundEffectManager playSoundNamed:@"SoundEffect.aiff"];
